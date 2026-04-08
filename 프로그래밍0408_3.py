0027404f6f0e4a3c050a45ef3ba294a66eb480d5
chae-yeon75 <limchaeyeon0705@gmail.com>
2026-04-08 17:39:45 +0900

    Add files via upload

---
+++ 프로그래밍0408_3.py
@@ -0,0 +1,24 @@
+'''
+#스왑
+a = 100
+b = 200
+print('Before swap;',a,b)
+a,b =b,a
+print('After swap:',a,b)
+
+a = 100
+b = 200
+print('Before swap;',a,b)
+a=b
+b=a
+print('After swap:',a,b)
+
+#-> 위에거 아래거 실행했을 때 같음
+'''
+
+#6-7 튜플의 연산
+#6-9 집합
+set1 = {0,1,2,2,4,5,5}
+print(set1)
+set2 = {0,5,2,2,4,1}
+print(set2)
